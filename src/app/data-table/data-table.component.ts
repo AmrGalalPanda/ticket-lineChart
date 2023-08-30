3aba243aa2870766b6d627ece78d5800ddfe2d1b
AmrGalalPanda <amr_galal2015@hotmail.com>
2023-08-30 17:03:42 +0300

    5th edit

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
- import * as $ from 'jquery';
-
+ declare var $: any;
 @Component({
   selector: 'app-data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent {
+  fullText = "سينما مصر , مول العرب , برج الأطنان , سرايا القبة المتفرعه من اي مكان";
+  truncatedText!: string;
 
+  ngOnInit(): void {
+    this.truncateText();
+    $(document).ready(() => {
+      
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+  }
+
+  truncateText() {
+    const words = this.fullText.split(' ');
+    if (words.length > 4) {
+      this.truncatedText = words.slice(0, 4).join(' ') + '...';
+    } else {
+      this.truncatedText = this.fullText;
+   
+    }
+  
+  }
+  
 }
